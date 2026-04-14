811f7fe41d3ef2d6b24eeec1691b225f69f8c7a1
koome <leonkoome4@gmail.com>
2026-04-14 11:47:22 +0300

    1

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var burgers = ['Hamburger', 'Cheeseburger'];
+var featuredDrink = 'Strawberry Milkshake';
+
+//  Function and Block
+function addBurger() {
+    var newBurger = 'Flatburger'; 
+    burgers.push(newBurger);
+}
+
+// Variable Declaration and Scope
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake';
+    }
+    console.log('Inside block - Featured Drink:', featuredDrink);
